989fd15bf3846f233c8f3731af5d2e061646b67d
LoubnaAbbar <loubna.abbar@edu.devinci.fr>
2026-02-05 12:30:07 +0100

    retirer wordpress

--- wp-content/themes/bap-theme/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function bap_setup() {
-  add_theme_support('title-tag');
-  register_nav_menus([
-    'primary' => 'Menu principal',
-  ]);
-}
-add_action('after_setup_theme', 'bap_setup');
--- wp-content/themes/bap-theme/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<div style="max-width:900px;margin:60px auto;font-family:system-ui;padding:0 20px;">
-  <h1>BAP Theme actif </h1>
-  <p>Si tu vois ce texte, c'est cool
-
-<?php get_footer(); ?>
